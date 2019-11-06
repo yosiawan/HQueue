b1d7c99dc848f5463a8fc3eb7227fa3ac4901680
Yosia <yosiasetiawan@outlook.com>
2019-11-06 14:41:08 +0700

    Initial Commit

---
+++ HQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HQueue
+//
+//  Created by Yosia on 06/11/19.
+//  Copyright © 2019 Apple Dev. Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
