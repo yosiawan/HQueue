d4a71478c57df75c4c4c7e7cad4001fbb30669f0
Yosia <yosiasetiawan@outlook.com>
2019-11-06 15:26:49 +0700

    defined initial VC

--- HQueue/AppDelegate.swift
+++ HQueue/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = UIViewController()
+        let mainViewController = ViewController()
         mainViewController.view.backgroundColor = UIColor.blue
         window.rootViewController = mainViewController
         window.makeKeyAndVisible()
