69a948f1480d1ce21a88ddfdb480a5311c1a2876
ridolud <faridho.lutfi@gmail.com>
2019-11-11 14:52:29 +0700

    tidak bisa init window baru di appdelegate

--- HQueue/AppDelegate.swift
+++ HQueue/AppDelegate.swift
@@ -15,11 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = ViewController()
-        mainViewController.view.backgroundColor = UIColor.blue
-        window.rootViewController = mainViewController
-        window.makeKeyAndVisible()
         return true
     }
 
