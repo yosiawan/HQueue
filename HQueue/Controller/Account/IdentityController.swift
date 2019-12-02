5ed9adffd8e377f988f2a671682fcc3a2597e993
Yosia <yosiasetiawan@outlook.com>
2019-12-02 17:09:53 +0700

    Added UIImagePicker

--- HQueue/Controller/Account/IdentityController.swift
+++ HQueue/Controller/Account/IdentityController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class IdentityController: UIViewController {
+class IdentityController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ktpImg: UIImageView!
+    var imgPickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.isModalInPresentation = true
         let rightBarButton = UIBarButtonItem(title: "Lanjut", style: .plain, target: self, action: #selector(self.nextAction))
         self.navigationItem.rightBarButtonItem = rightBarButton
+        
+        
     }
 
     @IBAction func nextAction(_ sender: Any) {
         let vc = CredentialController()
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imgPickerController =  UIImagePickerController()
+        imgPickerController.delegate = self
+        imgPickerController.sourceType = .camera
+
+        present(imgPickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imgPickerController.dismiss(animated: true, completion: nil)
+        ktpImg.image = info[.originalImage] as? UIImage
     }
 }
